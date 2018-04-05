9ee9d6a7d1a8c10b2c241c8fd387ba193b88f551
andrew <andrew@megsupporttools.com>
2018-04-06 00:42:08 +0200

    fix input method

--- input.js
+++ input.js
@@ -2,12 +2,12 @@ var keyDownAt = 0;
 
 function initKeyListeners(){
     document.body.onkeydown = function(e) {
-        if (e.char === 32 || e.char === 13 ) {
+        if (e.code === "Space" || e.code === "Enter" ) {
             keyDownAt = new Date().getTime();
         }
     };
     document.body.onkeyup = function(e) {
-        if (e.char === 32 || e.char === 13 ) {
+        if (e.code === "Space" || e.code === "Enter" ) {
             if ( keyDownAt !== 0 ){
                 var result = keyDownAt - new Date().getTime();
                 keyDownAt = 0;
