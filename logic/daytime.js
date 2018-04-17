86f5b10cdc08ffe10e582be9da703abc7f0ada6c
Andrew Dabski <jedrus1133@gmail.com>
2018-04-17 22:50:39 +0200

    proper working of day time

--- logic/daytime.js
+++ logic/daytime.js
@@ -13,6 +13,21 @@ function update_variables(){
     document.getElementById('lvl').innerHTML = game.state.lvl;
 }
 
+function daytime_robbery(){
+    hide("bttn_daytime_robbery");
+    game.state.gold += 100;
+    update_variables();
+}
+
+function daytime_shop(){
+    if(game.state.gold >= 75){
+        game.state.gold -= 75;
+        game.state.lvl += 1;
+        update_variables();
+    }
+}
+
 function end_of_day(){
     hide("div_daytime");
+    start_preparation();
 }
