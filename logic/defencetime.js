40833de1f4b48f2e61fe0768901affbb0377c6cb
Andrew Dabski <jedrus1133@gmail.com>
2018-04-17 22:58:02 +0200

    cycle of day

--- logic/defencetime.js
+++ logic/defencetime.js
@@ -1,4 +1,10 @@
 
 function start_defence(){
+    show('div_defencetime');
+}
 
+
+function end_of_defence(){
+    hide('div_defencetime');
+    start_defence();
 }
--- logic/preparetime.js
+++ logic/preparetime.js
@@ -1,4 +1,12 @@
 
 function start_preparation(){
+    document.getElementById('preparetime_hint').innerHTML += "</br>Enemy!";
+    show('div_preparetime');
+}
 
+
+
+function end_of_preparation(){
+    hide('div_preparetime');
+    start_defence();
 }
