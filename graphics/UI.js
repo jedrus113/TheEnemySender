db5090ab2c74f0e64d3faa32632421f620f17544
Andrew Dabski <jedrus1133@gmail.com>
2018-04-17 21:51:19 +0200

    cleanup

--- graphics/UI.js
+++ graphics/UI.js
@@ -6,16 +6,6 @@ function hide(id){
     document.getElementById(id).setAttribute("hidden", "True");
 }
 
-
-function drawUI(){
-    var waitTime = 0;
-    if (nextEnemyPossibleAt !== 0 && nextEnemyPossibleAt > Date.now()) {
-        waitTime = (nextEnemyPossibleAt - Date.now()) / 1000;
-    }
-    drawText(gameCanvas2d, "Wait " + waitTime, true, 20, 10, 20);
-
-}
-
 function showLoadingScreen(){
     show("loading_bar");
 }
