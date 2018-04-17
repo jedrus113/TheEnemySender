eddc3020cd140ebce24d2ffdfbd9b002624469b0
Andrew Dabski <jedrus1133@gmail.com>
2018-04-17 22:45:45 +0200

    set daytime actions

--- logic/daytime.js
+++ logic/daytime.js
@@ -2,4 +2,17 @@
 
 function start_day(){
     console.log("Start day " + game.state.day);
+    update_variables();
+    show("div_daytime");
+
+}
+
+function update_variables(){
+    document.getElementById('gold').innerHTML = game.state.gold;
+    document.getElementById('day').innerHTML = game.state.day;
+    document.getElementById('lvl').innerHTML = game.state.lvl;
+}
+
+function end_of_day(){
+    hide("div_daytime");
 }
